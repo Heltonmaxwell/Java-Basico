eda79ce230285d01a357a414b497368b91449bbd
heltonIniciante <heltonmaxwell.info@gmail.com>
2024-08-19 09:18:24 -0300

    Adicionando um exemplo de interface.

---
+++ edu/helton/dio/atividades/Interface/Calculadora.java
@@ -0,0 +1,27 @@
+package edu.helton.dio.atividades.Interface;
+
+/*No conceito de interface, o programa deve receber todos os métodos da interface, não importando a forma 
+ * em como esses métodos serão inseridos, mas por OBRIGATORIEDADE, deve possuir todos os métodos, pois a interface
+ * irá necessitar requisitar o método.
+ */
+
+public class Calculadora implements OperacaoMatematica{
+    
+    public void soma(double valor1, double valor2){
+
+    }
+
+    public void subtracao(double valor1, double valor2){
+
+    }
+
+    public void divisao(double valor1, double valor2){
+
+    }
+
+    public void multiplicacao(double valor1, double valor2){
+
+    }
+
+
+}
